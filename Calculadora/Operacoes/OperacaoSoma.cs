bf6a8b2ed07577d7478cf32845b9d7e792ecceea
NataFB <natafbt@gmail.com>
2026-02-04 13:56:32 -0300

    Criando Menu e Soma

---
+++ Calculadora/Operacoes/OperacaoSoma.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Calculadora.Operacoes
+{
+    class OperacaoSoma
+    {
+        public static void Soma()
+        {
+            List<double> numeros = new List<double>();
+
+            Console.WriteLine("Digite números para somar.");
+            Console.WriteLine("Pressione ENTER vazio para finalizar.\n");
+
+            while (true)
+            {
+                Console.Write("Digite um número: ");
+                string entrada = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(entrada))
+                {
+                    break;
+                }
+                else if (double.TryParse(entrada,CultureInfo.InvariantCulture, out double valor))
+                {
+                    numeros.Add(valor);
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido, tente novamente.");
+                }
+            }
+
+            double resultado = 0;
+
+            foreach (double n in numeros)
+            {
+                resultado += n;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Resultado da soma: {resultado.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Pressione ENTER para voltar ao menu");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,48 @@
+﻿using Calculadora.Operacoes;
+
+namespace Calculadora
+{
+    class Program
+    {
+
+        enum Menu
+        {
+            Soma = 1,
+            Subtracao,
+            Divisao,
+            Multiplicacao,
+            Potencia,
+            Raiz,
+            Sair
+        }
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            while (!escolheuSair) // Enquanto o usuário não(!) escolher sair exiba o menu
+            {
+                Console.WriteLine("Menu da calculadora, Selecione uma das opções:");
+                Console.WriteLine(
+                    "1-Soma\n" +
+                    "2-Subtração\n" +
+                    "3-Divisão\n" +
+                    "4-Multiplicação\n" +
+                    "5-Potencia\n" +
+                    "6-Raiz\n" +
+                    "7-Sair");
+                Menu opcao = (Menu)int.Parse(Console.ReadLine()); //Aplicando casting para o enum Menu
+
+                switch (opcao)
+                {
+                    case Menu.Soma:
+                        OperacaoSoma.Soma();
+                        break;
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+
+                }
+                Console.Clear(); // Limpa o console para melhor visualização
+            }
+        }
+    }
+}
