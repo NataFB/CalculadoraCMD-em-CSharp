09109a25f88d5543ba4e09fa6e0bee694143e66d
NataFB <natafbt@gmail.com>
2026-02-06 12:18:16 -0300

    Lançando o executável

--- Calculadora/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net10.0/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92bff7b030153910ecae8774999e735daca6580e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227d43e7bdd3ea0b1edf577c6b122b1c0eba7e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
